5b5f32df22ce322652f7077478a3a9f3bd3b1ffa
Matt <S3943492@student.rmit.edu.au>
2024-08-20 18:57:33 +1000

    added functionality for displaying Project Page

---
+++ script.js
@@ -0,0 +1,13 @@
+function displayProjectPage(){
+    let projectPage = document.getElementById("projectPage");
+    projectPage.style.zIndex = "0";
+    projectPage.style.top = "0vh";
+    
+}
+
+function displayHomePage(){
+    let projectPage = document.getElementById("projectPage");
+    projectPage.style.zIndex = "0";
+    projectPage.style.top = "100vh";
+    
+}
