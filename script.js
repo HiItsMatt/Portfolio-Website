6c220403f8534dc59b1b0e1e746a8b5994b2d9fe
Matt <S3943492@student.rmit.edu.au>
2024-08-21 08:50:09 +1000

    finished the content for the Projects Page

--- script.js
+++ script.js
@@ -9,5 +9,12 @@ function displayHomePage(){
     let projectPage = document.getElementById("projectPage");
     projectPage.style.zIndex = "0";
     projectPage.style.top = "100vh";
-    
+}
+
+function openOnlineCalendar(){
+    window.open("https://HiItsMatt.github.io/The-Online-Calendar");
+}
+
+function openOnlineProjectManager(){
+    window.open("https://github.com/HiItsMatt/The-Online-Project-Manager");
 }
